fd604187ea8683c988b09fb0428fd49b56458be0
HMUNACHI <ndubuakuhenry@gmail.com>
2025-04-25 08:24:16 +0100

    updated react chat example

--- examples/react-chat/utils/modelUtils.ts
+++ examples/react-chat/utils/modelUtils.ts
@@ -1,4 +1,4 @@
-import { initLlama, LlamaContext } from "cactus-rn-3";
+import { initLlama, LlamaContext } from "cactus-rn";
 import { Platform } from "react-native";
 import * as FileSystem from 'expo-file-system';
 
